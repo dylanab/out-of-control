37e0e2ed50d4dadc04ca93504be503e3b6043c5f
Cory Huckeba <cory.huckeba@gmail.com>
2020-07-10 19:34:11 -0400

    Add Card and CardDeck scripts and singleton package

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+    public int id;
+    public string name;
+    public string description;
+}
